fb77af12af2fe5db38f733f7bed2304f3d11db59
Biolumens <daisymerollingtheyhatingpatrol@gmail.com>
2022-10-29 19:45:48 -0500

    Added glint and status effects to skooma

--- src/main/java/net/glowy/ycs/item/Skooma.java
+++ src/main/java/net/glowy/ycs/item/Skooma.java
@@ -2,15 +2,17 @@ package net.glowy.ycs.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.glowy.ycs.YCS;
+import net.glowy.ycs.item.custom.SkoomaItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class Skooma {
 
     private static final Item SKOOMA = Registry.register(Registry.ITEM, new Identifier(YCS.MOD_ID, "skooma"),
-            new Item(new FabricItemSettings().group(ItemGroup.BREWING)));
+            new SkoomaItem(new FabricItemSettings().group(ItemGroup.BREWING).rarity(Rarity.EPIC)));
 
     public static void initialize(){
         YCS.LOGGER.debug("Loading Skooma!");
--- src/main/java/net/glowy/ycs/item/custom/SkoomaItem.java
+++ src/main/java/net/glowy/ycs/item/custom/SkoomaItem.java
@@ -1,4 +1,40 @@
 package net.glowy.ycs.item.custom;
 
-public class SkoomaItem {
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MilkBucketItem;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.world.World;
+
+public class SkoomaItem extends MilkBucketItem {
+    public SkoomaItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+            if (user instanceof ServerPlayerEntity) {
+                ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)user;
+                Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+                serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+            }
+            if (user instanceof PlayerEntity && !((PlayerEntity)user).getAbilities().creativeMode) {
+                stack.decrement(1);
+            }
+            if (!world.isClient) {
+                user.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 1800, 1));
+                user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 1800, 1));
+            }
+            return stack;
+        }
+
+    @Override
+    public boolean hasGlint(ItemStack stack) {
+        return true;
+    }
 }
